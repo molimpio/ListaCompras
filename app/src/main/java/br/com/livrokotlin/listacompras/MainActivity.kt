1b21b908441191c974352c67431182f984023447
Moises <moises@rcbdesenvolvimento.com.br>
2018-10-05 20:23:22 -0300

    adicionando produtos na lista

---
+++ app/src/main/java/br/com/livrokotlin/listacompras/MainActivity.kt
@@ -0,0 +1,23 @@
+package br.com.livrokotlin.listacompras
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val produtosAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1)
+        lv_produtos.adapter = produtosAdapter
+
+        bt_adicionar.setOnClickListener {
+            val produto = et_produto.text.toString()
+            produtosAdapter.add(produto)
+
+        }
+    }
+}
